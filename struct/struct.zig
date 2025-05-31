e25063dc08f7776d0157fa97947002ef4d1d983a
wiktrek <wiktor.janiszewski10@gmail.com>
2025-05-31 16:51:25 +0200

    relearning structs

--- struct/struct.zig
+++ struct/struct.zig
@@ -1,13 +1,13 @@
 const std = @import("std");
 const print = std.debug.print;
 pub fn main() !void {
-    const Person = struct {
-        name: []const u8,
-        age: u8,
+    const User = struct {
+        username: []const u8,
+        password: []const u8,
     };
-    const wiktrek = Person{
-        .name = "wiktrek",
-        .age = 100,
+    const wiktrek = User{
+        .username = "wiktrek",
+        .password = "verystrongpassword",
     };
-    print("name: {s}\nage: {}", .{ wiktrek.name, wiktrek.age });
+    print("username: {s}\npassword: {s}", .{ wiktrek.username, wiktrek.password });
 }
